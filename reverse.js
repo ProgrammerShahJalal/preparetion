11b6d999869f70a27e095b8386b9779d8b52a81c
Md Shah Jalal <bestwebdesignerr@gmail.com>
2021-12-23 12:13:02 +0600

    perseflote

--- reverse.js
+++ reverse.js
@@ -18,6 +18,22 @@ function reversedNumber(num) {
 }
 console.log(reversedNumber(12345));
 
+/*------ here:
+1. num.toString() converts the given number into string so that we can use split function on it next.
+
+2. num.split("") converts the string into an array of characters so that we can use array reverse function (which doesn't work on a string.)
+
+3. num.reverse() reverse the order of the items in the array.
+
+4. num.join('') reassemble the reversed characters into a string.
+
+5. parseFloat(num) converts num into a float from a String.
+Note: parseFloat runs in the end (even though it is on the first line of the function) on the reversed number and removes any leading zeroes. 
+
+6. num * Math.sign(num) multiplies the number with the sign of the original number provided.
+
+--------*/
+
 
 
 
